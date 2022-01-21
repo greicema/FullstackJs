d4dd8236f5b598ae717e3ca963fbea534cfb93d2
greicemsj <sgreicemaraj@gmail.com>
2022-01-21 20:06:05 -0300

    correção de espaço

--- JS Dia 2 - Condicionais/Exercício - Utilizando condicionais/script.js
+++ JS Dia 2 - Condicionais/Exercício - Utilizando condicionais/script.js
@@ -1,7 +1,7 @@
 let pilotName = prompt("What's your name, pilot?")
 let spaceshipVelocity = 0
 newSpaceshipVelocity = prompt("Which velocity would you like to travel?")
-velocityConfirmation = confirm("You are traveling at " + newSpaceshipVelocity + " km/s. Are you sure?")
+velocityConfirmation = confirm("You are traveling at " + newSpaceshipVelocity + "km/s. Are you sure?")
 
 if (velocityConfirmation) {
     spaceshipVelocity = newSpaceshipVelocity
@@ -19,4 +19,4 @@ if (spaceshipVelocity <= 0) {
     alert("Dangerous speed. Forced automatic control.")
 }
 
-alert("Pilot: " + pilotName + "\nVelocity: " + newSpaceshipVelocity + " km/s")
+alert("Pilot: " + pilotName + "\nVelocity: " + newSpaceshipVelocity + "km/s")
