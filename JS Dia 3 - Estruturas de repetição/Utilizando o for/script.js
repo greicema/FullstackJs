d37d010abe4be5daac2a443586d9406c54222ff7
greicemsj <sgreicemaraj@gmail.com>
2022-01-26 20:06:43 -0300

    resolução

--- JS Dia 3 - Estruturas de repetição/Utilizando o for/script.js
+++ JS Dia 3 - Estruturas de repetição/Utilizando o for/script.js
@@ -1,4 +1,14 @@
 let shipName = prompt("Enter your ship's name")
 let shipLetter = prompt("Which letter would you like to change?")
-let newShipLÇetter = prompt("Which letter would you like to replace " + shipLetter.toUpperCase() + " it with?")
+let newShipLetter = prompt("Which letter would you like to replace " + shipLetter.toUpperCase() + " it with?")
 let newShipName = ""
+
+for (let i = 0; i < shipName.length; i++) {
+    if (shipName[i] == shipLetter) {
+        newShipName += newShipLetter
+    } else {
+        newShipName += shipName[i]
+    }
+}
+
+alert("The new name of the ship is " + newShipName.toLocaleUpperCase())
