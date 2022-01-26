ec68fe7793bd7eafad8d57230018103443ed7f63
greicemsj <sgreicemaraj@gmail.com>
2022-01-26 20:01:33 -0300

    variáveis

---
+++ JS Dia 3 - Estruturas de repetição/Utilizando o for/script.js
@@ -0,0 +1,4 @@
+let shipName = prompt("Enter your ship's name")
+let shipLetter = prompt("Which letter would you like to change?")
+let newShipLÇetter = prompt("Which letter would you like to replace " + shipLetter.toUpperCase() + " it with?")
+let newShipName = ""
