826df4e49c54e8bfdcbeefdd8e373976a8fccb84
greicemsj <sgreicemaraj@gmail.com>
2022-01-25 21:14:41 -0300

    variáveis

---
+++ JS Dia 2 - Condicionais/Exercício - Utilizando condicionais/Fixando switch/script.js
@@ -0,0 +1,5 @@
+let distanceLightYears = prompt("Enter a distance in light-years")
+
+let convertedDistancePC = (distanceLightYears * 0.306601)
+let convertedDistanceAU = (distanceLightYears * 63241.1)
+let convertedDistanceKM = (distanceLightYears * 9.5 * Math.pow(10, 12))
