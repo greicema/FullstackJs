84c629790d900e1fdd8e90536177665ed2401933
greicemsj <sgreicemaraj@gmail.com>
2022-01-26 18:38:15 -0300

    breves ajustes

--- Utilizando o while/script.js
+++ Utilizando o while/script.js
@@ -1,11 +1,12 @@
 let chosenOption = ""
 let optionCount = 0
 let shipName = prompt("Enter your ship's name")
-let chosenOption = prompt("Do you want to go into space fold?\n1 - Yes\n2 - No")
+
+chosenOption = prompt("Do you want to go into space fold?\n1 - Yes\n2 - No")
 
 while (chosenOption == "1") {
     optionCount++
     chosenOption = prompt("Do you want to go into the next space fold?\n1 - Yes\n2 - No")
 }
 
-alert("Spaceship: " + shipName + "Number of space folds: " + optionCount)
+alert("Spaceship: " + shipName + "\n'Number of space folds: " + optionCount)
