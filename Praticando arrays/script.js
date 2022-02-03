57379d0e48f177483c5c0398a9b52177eca38b33
greicemsj <sgreicemaraj@gmail.com>
2022-02-03 11:32:53 -0300

    resolução

--- Praticando arrays/script.js
+++ Praticando arrays/script.js
@@ -1,7 +1,26 @@
 const hitchedSpaceships = [
-    ["Fenix", 8, true],
-    ["Golias", 10, true],
-    ["Helmet, 5, false"],
-    ["Elemental", 3, false],
-    ["Darwin", 15, false]
+    [" Fenix", 8, true],
+    [" Golias", 10, true],
+    [" Helmet", 5, false],
+    [" Elemental", 3, false],
+    [" Darwin", 15, false]
 ]
+
+let with9Passangers = hitchedSpaceships.filter(spaceship => {
+    return spaceship[1] > 9
+}).map(spaceship => {
+    return spaceship[0]
+})
+console.log("Spaceships with greater than 9 passangers: " + with9Passangers)
+
+let noHitchedSpaceships = hitchedSpaceships.findIndex(spaceship => {
+    return spaceship[2] == false
+})
+console.log("Not hitched first spaceship platform: " + (noHitchedSpaceships + 1))
+
+let upperSpaceships = hitchedSpaceships.map(spaceship => {
+    return spaceship[0].toUpperCase()
+})
+console.log("Upper cased spaceships: " + upperSpaceships)
+
+alert("Spaceships with greater than 9 passangers: " + with9Passangers + "\nNot hitched first spaceship platform: " + (noHitchedSpaceships + 1) + "\nUpper cased spaceships: " + upperSpaceships)
