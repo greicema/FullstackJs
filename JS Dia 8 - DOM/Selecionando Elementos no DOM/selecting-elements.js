67ad25655084b0639ccf91bef85c2b20aaf23f0e
greicemsj <sgreicemaraj@gmail.com>
2022-02-22 19:47:51 -0300

    seleção de elementos

---
+++ JS Dia 8 - DOM/Selecionando Elementos no DOM/selecting-elements.js
@@ -0,0 +1,13 @@
+function show() {
+    /*let elements = document.getElementsByName("phone")
+    console.log(elements[0].value)
+
+    let element = document.getElementById("name")
+    console.log(element.value) 
+
+    let otherElements = document.querySelectorAll("div#phones input[name='phone']")
+    console.log(otherElements)*/
+
+    let elements = document.getElementsByTagName("input")
+    console.log(elements)
+}
