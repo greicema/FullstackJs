7d08d16360fe43a036b7eafd03b5b9d01bdfe381
greicemsj <sgreicemaraj@gmail.com>
2022-02-22 18:21:00 -0300

    alguns exemplos de date

--- JS Dia 7 - POO/Math e Date/script.js
+++ JS Dia 7 - POO/Math e Date/script.js
@@ -7,3 +7,11 @@ console.log(Math.PI)
 console.log(Math.sqrt(64))
 
 //DATE
+let myBirthday = new Date(2019, 03, 29)
+console.log(myBirthday)
+
+myBirthday = new Date(2019, 0, 29) //No DATE o primeiro mês é 0
+console.log(myBirthday)
+
+let today = Date.now()
+console.log(today)
