6543b2a651dda564e5e641c23f99a567e911276b
greicemsj <sgreicemaraj@gmail.com>
2022-02-22 18:39:13 -0300

    eventos html

---
+++ JS Dia 7 - POO/Eventos no HTML/events.js
@@ -0,0 +1,19 @@
+function clickMe() {
+    console.log("Clicked!")
+}
+
+function focusMe() {
+    console.log("Focused!")
+}
+
+function blurMe() {
+    console.log("Blurred!")
+}
+
+function changeMe() {
+    console.log("Changed!")
+}
+
+function overMe() {
+    console.log("Mouse here!")
+}
